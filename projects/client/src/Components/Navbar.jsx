4ee20a321a0d66df4d43b2a6943a2f2b1e86676e
Harris011 <johandaharris@gmail.com>
2023-03-28 13:27:30 +0700

    FIN-2-2: Update Navbar

--- projects/client/src/Components/Navbar.jsx
+++ projects/client/src/Components/Navbar.jsx
@@ -11,54 +11,100 @@ import {
     InputRightElement,
     InputGroup,
     Link,
-    Box
+    Text
  } from '@chakra-ui/react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { GoSearch } from 'react-icons/go';
 import { BsCart2 } from 'react-icons/bs';
 
 function Navbar() {
+    const name = '';
+    const roleId = '';
     return ( 
-    <Container maxW={'full'} px={{base:'4', sm:'8', md:'12', lg:'24'}} bgGradient='linear(to-b, orange.500, orange.400)'>
+    <Container maxW={'full'} px={{base:'2', sm:'6', md:'10', lg:'24'}} bgGradient='linear(to-b, orange.500, orange.400)'>
         <Flex py={'3'} alignItems={'Center'} justifyContent={'space-between'}>
-            <Flex>
-                <Link textDecoration={'none'} _hover={{ textDecoration: "none" }} fontSize={{base:'xl', md:'xl', lg:'3xl'}} fontWeight='semibold' color={'white'}>Grocery</Link>
-            </Flex>
-            <InputGroup width={'50%'} size={'md'}>
-                <Input pr={'2.75rem'}  placeholder='search' size="sm" type={'search'} rounded={'lg'} backgroundColor={'white'}/>
-                <InputRightElement h={'2rem'}>
-                    <Button borderLeftRadius={'0'} h={'2rem'} size={'sm'}>
-                        <GoSearch size={'sm'}/>
-                    </Button>
-                </InputRightElement>
-            </InputGroup>
-            <Flex alignItems={'Center'} gap={'1'}>
-                <Flex
-                display={['none', 'none','flex', 'flex']} alignItems={'center'}
-                >
-                    <Box>
-                        <Link p={'2'} textDecoration={'none'} _hover={{ textDecoration: "none", rounded:'lg' }} fontSize={{base:'xl', md:'xl', lg:'2xl'}} fontWeight='semibold' color={'white'}>Sign Up</Link>
-                        <Link p={'2'} textDecoration={'none'} _hover={{ textDecoration: "none", rounded:'lg' }} fontSize={{base:'xl', md:'xl', lg:'2xl'}} fontWeight='semibold' color={'white'}>Login</Link>
-                    </Box>
-                    <Link variant={'outline'} backgroundColor={'transparent'} border={'none'} color={'white'} fontSize={{base:'xl', md:'xl', lg:'2xl'}} p='1'>
+            <Link textDecoration={'none'} _hover={{ textDecoration: "none" }} fontSize={{base:'lg', md:'xl', lg:'3xl'}} fontWeight='semibold' color={'white'}>FreshFinds</Link>
+            {
+                name ? 
+                (roleId == 3 ?
+                <Flex alignItems={'Center'} gap={{base:'1', sm:'1', md:'5', lg:'5'}} justifyContent='space-between' width={{md:'md', lg:'6xl'}} px={{base:'1', sm:'2', md:'10', lg:'10'}}>
+                    <InputGroup size={'md'}>
+                        <Input pr={'2.75rem'}  placeholder='search' size="sm" type={'search'} rounded={'lg'} backgroundColor={'white'}/>
+                        <InputRightElement h={'2rem'}>
+                            <Button borderLeftRadius={'0'} h={'2rem'} size={'sm'}>
+                                <GoSearch size={'sm'}/>
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                    <Link variant={'outline'} backgroundColor={'transparent'} border={'none'} color={'white'} fontSize={{base:'xl', md:'xl', lg:'2xl'}} p='1' mx='0'>
                         <BsCart2/>
                     </Link>
                 </Flex>
-                <Flex display={['flex', 'flex', 'none', 'none']} alignItems={'center'} gap={'1'}>
-                    <Menu>
-                        <MenuButton as={Button} variant={'outline'} rounded={'lg'} size={'sm'} color={'orange.400'} backgroundColor={'white'}>
-                          <AiOutlineMenu/>
-                        </MenuButton>
-                        <MenuList>
-                          <MenuItem type='button'>Sign Up</MenuItem>
-                          <MenuItem type='button'>Login</MenuItem>
-                        </MenuList>
-                    </Menu>
-                    <Link variant={'outline'} backgroundColor={'transparent'} border={'none'} color={'white'} fontSize={{base:'xl', md:'xl', lg:'2xl'}} p='1'>
+                :
+                null)
+                :
+                <Flex alignItems={'Center'} gap={{base:'1', sm:'1', md:'5', lg:'5'}} justifyContent='space-between' width={{md:'md', lg:'2xl'}} pl={{base:'2',lg:'16'}}>
+                    <InputGroup size={'md'}>
+                        <Input pr={'2.75rem'}  placeholder='search' size="sm" type={'search'} rounded={'lg'} backgroundColor={'white'}/>
+                        <InputRightElement h={'2rem'}>
+                            <Button borderLeftRadius={'0'} h={'2rem'} size={'sm'}>
+                                <GoSearch size={'sm'}/>
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                    <Link variant={'outline'} backgroundColor={'transparent'} border={'none'} color={'white'} fontSize={{base:'xl', md:'xl', lg:'2xl'}} p='1' mx='3'>
                         <BsCart2/>
                     </Link>
                 </Flex>
-            </Flex>
+            }
+            {
+                name ? 
+                <Flex alignItems={'Center'} gap={'1'}>
+                    <Flex
+                    display={['none', 'none','flex', 'flex']} alignItems={'center'} ml='1'
+                    >
+                        <Menu>
+                          <MenuButton>
+                            <Text color='white' p={'1'} fontSize='2xl' fontWeight={'semibold'}>{name}</Text>
+                          </MenuButton>
+                          <MenuList>
+                            <MenuItem type='button'>Logout</MenuItem>
+                          </MenuList>
+                        </Menu>
+                    </Flex>
+                    <Flex display={['flex', 'flex', 'none', 'none']} alignItems={'center'} ml='1'>
+                        <Menu>
+                          <MenuButton>
+                            <Text color='white' p={'1'} fontSize='xl' fontWeight={'semibold'}>{name}</Text>
+                          </MenuButton>
+                          <MenuList>
+                            <MenuItem type='button'>Logout</MenuItem>
+                          </MenuList>
+                        </Menu>
+                    </Flex>
+                </Flex>
+                :
+                <Flex alignItems={'Center'} gap={'1'}>
+                    <Flex
+                    display={['none', 'none','flex', 'flex']} alignItems={'center'}
+                    w='auto'
+                    >
+                        <Link p={'1'} mr='2' textDecoration={'none'} _hover={{ textDecoration: "none", rounded:'lg' }} fontSize={{base:'xl', md:'xl', lg:'2xl'}} fontWeight='semibold' color={'white'} >SignUp</Link>
+                        <Link p={'1'} textDecoration={'none'} _hover={{ textDecoration: "none", rounded:'lg' }} fontSize={{base:'xl', md:'xl', lg:'2xl'}} fontWeight='semibold' color={'white'}>Login</Link>
+                    </Flex>
+                    <Flex display={['flex', 'flex', 'none', 'none']} alignItems={'center'} gap={'1'}>
+                        <Menu>
+                            <MenuButton as={Button} variant={'outline'} rounded={'lg'} size={'sm'} color={'orange.400'} backgroundColor={'white'}>
+                              <AiOutlineMenu/>
+                            </MenuButton>
+                            <MenuList>
+                              <MenuItem type='button'>Sign Up</MenuItem>
+                              <MenuItem type='button'>Login</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </Flex>
+                </Flex>
+            }
         </Flex>
     </Container> );
 }
