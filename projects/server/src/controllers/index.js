a3ee4dcf0e4363633e341bc84a56f98ad7c3814e
Harris011 <johandaharris@gmail.com>
2023-04-11 13:26:41 +0700

    develop-6: fix after merge

--- projects/server/src/controllers/index.js
+++ projects/server/src/controllers/index.js
@@ -2,6 +2,7 @@ const productController = require('./productController');
 const userController = require("./userController");
 const adminController = require("./adminController");
 
+
 module.exports = {
     productController,
     userController,
